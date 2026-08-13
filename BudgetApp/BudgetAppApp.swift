045c7441357415cc35ccb43d182554a215b6c9c2
Abdalwahed Moshref <abdalwahedmoshref1512@gmail.com>
2026-08-13 14:47:49 +0300

    Initial Commit

---
+++ BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by Abdalwahed Moshref on 13/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
